984471fab66cbab378cf44ba20fbc1eec798ca2d
leefowler <fowler.lee8@gmail.com>
2021-02-25 08:00:49 +0100

    sixth commit

--- can-sum.js
+++ can-sum.js
@@ -1,13 +1,17 @@
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return true;
   if (targetSum < 0) return false;
 
   for (let num of numbers) {
     const remainder = targetSum - num;
-    if (canSum(remainder, numbers) === true) {
+    if (canSum(remainder, numbers, memo) === true) {
+      memo[targetSum] = true;
       return true;
     }
   }
+
+  memo[targetSum] = false;
   return false;
 };
 
@@ -15,4 +19,4 @@ console.log(canSum(7, [2, 3])); // true
 console.log(canSum(7, [5, 3, 4, 7])); //true
 console.log(canSum(7, [2, 4])); //false
 console.log(canSum(8, [2, 3, 5])); //true
-console.log(canSum(300, [7, 14])); //false
+console.log(canSum(3000, [7, 14])); //false
